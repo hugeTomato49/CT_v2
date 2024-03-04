edeff50adf3634fcb336d0354dd86f52d7015587
IDG_Tomato <kevinmeng@zju.edu.cn>
2024-03-04 20:08:35 +0800

    -

--- src/components/table/LinkColumn.vue
+++ src/components/table/LinkColumn.vue
@@ -10,18 +10,21 @@
 
 <script>
 import { useStore } from 'vuex'
+import { computed } from 'vue'
 export default {
     name: 'LinkColumn',
     props: ['level'],
     setup(props) {
-        //selectionTree，遍历当前层(props.level)的结点，拿到父子id对， id1, id2
+        //selectionTree，遍历当前层(props.level)的结点，拿到父子id对列表， id1, id2
 
         //document.getElementById 拿到父子对id对应的TSCard id是card + id
         //element.offset 拿每个card的左上角的绝对坐标
-         //document.getElementByClassName 获取table的绝对坐标作为参考系坐标 并且利用vuex中的cardHeight, plotWidth计算线两个端点在svg中的坐标, svg的宽度是20px
-         
+        //document.getElementByClassName 获取table的绝对坐标作为参考系坐标 并且利用vuex中的rowHeight, plotWidth, columnWidth等必要的尺寸计算线两个端点在svg中的坐标, svg的宽度是20px
+        //和开会讨论不同的是，用rowHeight代替cardHeight
 
         const store = useStore()
+        const selectionTree = computed(()=>store.getters["tree/selectionTree"])
+
 
     
         
