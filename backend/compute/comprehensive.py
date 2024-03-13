da465e64a9fc4fdbc066ee75dc16aa375dd23f14
Nemo <2402214215@qq.com>
2024-03-13 21:27:50 +0800

    update: SD

--- backend/compute/comprehensive.py
+++ backend/compute/comprehensive.py
@@ -2,11 +2,14 @@
 @Description: 
 @Author: 
 @Date: 2024-03-13 19:51:25
-@LastEditTime: 2024-03-13 19:52:34
+@LastEditTime: 2024-03-13 21:25:48
 @LastEditors: Nemo
 '''
 from compute.TSfuncCal import GetMax, GetMin
 from compute.filter import getTSdata
+from compute.jsonTransfer import TSjson_exp
+import numpy as np
+
 def getBoundary(pv_tree_data, folder_path, timeRange=[]):
     level_max=[]
     level_min=[]
@@ -30,3 +33,25 @@ def getBoundary(pv_tree_data, folder_path, timeRange=[]):
         result.append(tmp_dict)
     return {'result':result}
 
+def getSDALL(pv_tree_data, folder_path, max_level=3):
+    data01 = getTSdata(pv_tree_data[0]['node_name'], folder_path)
+    _, TS_datalist = TSjson_exp({'data':data01})
+    Time_stamps = TS_datalist[:, 0]
+    result = []
+    for i in range(1, max_level):
+        level_data = []
+        for j in range(len(Time_stamps)):
+            level_data.append([])
+        for node in pv_tree_data:
+            if node['level'] == i+1:
+                _, node_TS_values = TSjson_exp({'data':getTSdata(node['node_name'], folder_path)})
+                node_TS_values = node_TS_values[:, 1]
+                for k in range(len(node_TS_values)):
+                    level_data[k].append(node_TS_values[k])
+        level_SD = [np.std(data) for data in level_data]
+        level_dict = {"level":i+1, "SD":[]}
+        for j in range(len(Time_stamps)):
+            time_value = {"Time": Time_stamps[j], "Value": level_SD[j]}
+            level_dict['SD'].append(time_value)
+        result.append(level_dict)
+    return result
