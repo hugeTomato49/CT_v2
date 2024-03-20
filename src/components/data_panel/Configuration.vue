51f9d9c67a03bdd604b7e9c1b20a31cf0f60bb6c
IDG_Tomato <kevinmeng@zju.edu.cn>
2024-03-20 21:37:40 +0800

    -

--- src/components/data_panel/Configuration.vue
+++ src/components/data_panel/Configuration.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container w-full h-full ">
-    <div class="data-set w-full h-1/2">
+  <div class="container w-full h-full p-2">
+    <div class="data-set w-full h-1/3">
       <div class="title text-lg  text-[#4B99D0] text-center">
-        <text>DataSet</text>
+        <div class="text-base">DataSet</div>
         <div class="border-solid border-1 border-light-800 mt-2"></div>
       </div>
-      <div class="flex flex-row w-full h-1/2 justify-around mt-5">
+      <div class="flex flex-row w-full h-1/2 justify-around mt-3">
         <div class="w-5/12 h-1/2 border-solid border-light-700 border-2"></div>
         <div class="w-5/12 h-1/2 border-solid border-light-700 border-2"></div>
       </div>
@@ -13,33 +13,33 @@
     </div>
     <div class="title configuration w-full h-1/2 flex-column">
       <div class="w-full text-lg text-[#4B99D0] text-center" >
-        <text>Configuration</text>
+        <div class="text-base">Configuration</div>
         <div class="border-solid border-1 border-light-800 mt-2"></div>
       </div>
-      <div class="flex flex-row w-full mt-6 ml-3">
-        <div class="w-1/10 text-[#4B99D0] text-2xl">K</div>
+      <div class="flex flex-row w-full mt-1 justify-center items-center">
+        <div class="w-1/10 text-[#4B99D0] text-sm">K</div>
         <div class="w-4/5 ml-2">
           <a-row>
-            <a-col :span="12">
-              <a-slider v-model:value="inputValue" :min="1" :max="50" />
+            <a-col :span="11">
+              <a-slider v-model:value="inputValue" :min="1" :max="20" />
             </a-col>
-            <a-col :span="4">
+            <a-col :span="1">
               <a-input-number
                 v-model:value="inputValue"
                 :min="1"
                 :max="20"
-                style="margin-left: 16px"
-                class="text-base"
+                style="margin-left: 10px"
+                class="text-"
                 onChangeComplete="onChange"
               />
             </a-col>
           </a-row>
         </div>
       </div>
-      <div class="w-full h-full flex flex-col">
-          <div class="w-full flex flex-row justify-center">
-              <div class="w-2/3 flex flex-row ">
-                  <div class="name " :style="{ 'color': themeColor }"> link </div>
+      <div class="w-full h-full flex flex-col px-2">
+          <div class="w-full flex flex-row justify-center mt-2">
+              <div class="w-full flex flex-row">
+                  <div class="name text-sm" :style="{ 'color': themeColor }"> link </div>
                   <div class="flex-1"></div>
                   <div v-if="linkVisible" @click="toggleLinkVisible" class="cursor-pointer">
                       <font-awesome-icon :icon="['fas', 'eye']" />
@@ -50,8 +50,8 @@
               </div>
           </div>
           <div class="w-full flex flex-row justify-center mt-2">
-              <div class="w-2/3 flex flex-row">
-                  <div class="name " :style="{ 'color': themeColor }"> highlight </div>
+              <div class="w-full flex flex-row">
+                  <div class="name text-sm" :style="{ 'color': themeColor }"> highlight </div>
                   <div class="flex-1"></div>
                   <div v-if="highlightVisible" @click="toggleHighlightVisible" class="cursor-pointer">
                       <font-awesome-icon :icon="['fas', 'eye']" />
@@ -63,11 +63,6 @@
           </div>
       </div>   
     </div>
-
-
-
-
-
   </div>
 </template>
 
